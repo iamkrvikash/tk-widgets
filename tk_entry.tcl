534ce57deb52a2a8dedc15b39d99c845630e0455
iamkrvikash <iamkrvikash@gmail.com>
2022-01-08 20:26:54 +0530

    tk_entry added

---
+++ tk_entry.tcl
@@ -0,0 +1,4 @@
+#!/usr/bin/wish
+
+grid [entry .myEntry -background red -foreground white -relief ridge -borderwidth 8 -font {Helvetica -18 bold} -width 35 -textvariable myvariable -justify right ]
+set myvariable "Helloo Worldd"
